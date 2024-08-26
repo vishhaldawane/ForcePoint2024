06a835647dea6368956292a222a46350c46b89d8
vishhaldawane <vishalviniing@gmail.com>
2024-08-26 10:19:51 +0530

    PtrToInt

---
+++ PointerToObject.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+//char        int        float       doube
+// 'A'        123        54.12       32322.33
+//00000000    0000 0000  0000 0000   0000 0000 0000 0000
+//            0000 0000  0000 0000   0000 0000 0000 0000
+//                       0000 0000   0000 0000 0000 0000 
+//                       0000 0000   0000 0000 0000 0000
+//    1         2             4            8
+//  65230      65330       65440      65980 <-unsinged int
+// ptr
+
+//int the data type of num
+    int num=190;
+
+//below int is NOT the data type of ptr
+//*ptr's data type is unsinged int ( cause of positive
+//memory address)
+//here int is the type of that variables
+//whose address is hold by the ptr
+
+    int *ptr;
+
+    ptr = &num;
+
+//value of a variable is accessible via
+//1. its name [ direct ]
+//2. its address [ indirect ]
+/*
+        name        num         ptr  <-- developer
+        value       190         64240 <--developer  
+        address     64240       64390 <--system
+*/
+    cout<<"\nnum is : "<<num;
+    cout<<"\nnum is : "<<*&num;
+    printf("\nnum stored at %ld",&num);
+
+   // cout<<"\nnum is : "<<*ptr;
+   // cout<<"\nptr is : "<<ptr;
+
+}
