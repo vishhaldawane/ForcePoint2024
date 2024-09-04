fdeec329fb0b49215cb4dd4daad2ecbed8aaf0ff
vishhaldawane <vishalviniing@gmail.com>
2024-09-04 10:46:56 +0530

    userManagement.sh

---
+++ userManagement.sh
@@ -0,0 +1,10 @@
+echo "		USER MANAGEMENT"
+echo "		----------------"
+echo "		1. Create User"
+echo "		2. View User By ID"
+echo "		3. View All Users"
+echo "		4. Modify User"
+echo "		5. Delete User By ID"
+echo "		-----------------"
+echo "		6. Exit	"
+
